504eb67027755a248eb9b9baa3b0cc49f1c345c0
Pedro Gonzalez Ferrandez <pdro74@hotmail.com>
2015-12-27 17:14:23 +0100

    bug solved. Works

--- app/src/main/java/com/pgfmusic/simplecontentprovider/Contract.java
+++ app/src/main/java/com/pgfmusic/simplecontentprovider/Contract.java
@@ -11,7 +11,7 @@ public class Contract {
     static final String STUDENTS_TABLE_NAME = "students";
 
     public static final String SCHEME = "content://";
-    public static final String AUTHORITY = "com.pgfmusic.simplecontentprovider";//.College";
+    public static final String AUTHORITY = "com.pgfmusic.simplecontentprovider";
     public static final String BASE_URI = SCHEME + AUTHORITY;
     public static final String PATH = "/students";
 }
--- app/src/main/java/com/pgfmusic/simplecontentprovider/MainActivity.java
+++ app/src/main/java/com/pgfmusic/simplecontentprovider/MainActivity.java
@@ -8,11 +8,14 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btnAddName = (Button) findViewById(R.id.btn_add_name);
         btnRetrStudents = (Button) findViewById(R.id.btn_retrieve_students);
+        tvResults = (TextView) findViewById(R.id.tv_results);
         btnAddName.setOnClickListener(this);
         btnRetrStudents.setOnClickListener(this);
     }
@@ -42,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         values.put(StudentsProvider.GRADE,
                 ((EditText)findViewById(R.id.et_grade)).getText().toString());
 
-        // TODO: 25/12/15 this doesn't work: "java.lang.IllegalArgumentException: Unknown URL content://com.pgfmusic.simplecontentprovider.College/students"
         Uri uri = getContentResolver().insert(
                 StudentsProvider.CONTENT_URI, values);
 
@@ -51,21 +54,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onClickRetrieveStudents() {
-        Toast.makeText(this, "retrieve", Toast.LENGTH_SHORT).show();
         // Retrieve student records
         String URL = Contract.BASE_URI + Contract.PATH;
-
+        Log.i(Contract.TAG, "Retrieving: " + URL);
         Uri students = Uri.parse(URL);
         Cursor c = getContentResolver().query(students, null, null, null, "name");
 
+        ArrayList<String> results = new ArrayList<>();
+        // Usually you want a ListView to show the data. Using a TextView for simplicity
+
         if (c.moveToFirst()) {
-            do{
-                Toast.makeText(this,
-                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
-                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
-                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
-                        Toast.LENGTH_SHORT).show();
+            do {
+                results.add(c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                        ", " + c.getString(c.getColumnIndex(StudentsProvider.NAME)) +
+                        ", " + c.getString(c.getColumnIndex(StudentsProvider.GRADE)));
             } while (c.moveToNext());
+            String text = "";
+            for (String result: results) {
+                text += result + "\n";
+            }
+            tvResults.setText(text);
+        } else {
+            Log.i(Contract.TAG, "Cursor not working");
         }
     }
 
